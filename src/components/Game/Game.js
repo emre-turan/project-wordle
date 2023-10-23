dd2c8ac05f4d63b05f3b7edfdc35c893c4b6400e
Emre Turan <itsemreturan@gmail.com>
2023-10-23 20:32:44 +0300

    completed task

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,11 @@
-import React from 'react';
-
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import GuessInput from "../GuessInput/GuessInput";
+import GuessResults from "../GuessResults/GuessResults";
+import { useState } from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import WonBanner from "../WonBanner/WonBanner";
+import LostBanner from "../LostBanner/LostBanner";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +13,32 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [gameStatus, setGameStatus] = useState("running");
+  const [guesses, setGuesses] = useState([]);
+
+  function handleSubmitGuesses(tentativeGuess) {
+    const nextGuesses = [...guesses, tentativeGuess];
+    setGuesses(nextGuesses);
+
+    if (tentativeGuess.toUpperCase() === answer) {
+      setGameStatus("won");
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus("lost");
+    }
+  }
+
+  return (
+    <>
+      {gameStatus}
+      <GuessResults guesses={guesses} answer={answer} />
+      <GuessInput
+        handleSubmitGuesses={handleSubmitGuesses}
+        gameStatus={gameStatus}
+      />
+      {gameStatus === "won" && <WonBanner numOfGuesses={guesses.length} />}
+      {gameStatus === "lost" && <LostBanner answer={answer} />}
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function GuessInput({ handleSubmitGuesses, gameStatus }) {
+  const [tentativeGuess, setTentativeGuess] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    handleSubmitGuesses(tentativeGuess);
+    setTentativeGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        disabled={gameStatus !== "running"}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        id="guess-input"
+        type="text"
+        value={tentativeGuess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setTentativeGuess(nextGuess);
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
